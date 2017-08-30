ae5e8e3de485b2287a7bd04db4f531db17c7fa84
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-08-30 09:45:27 +0200

    OPrette kaffe klasse og lavet to unit test.

---
+++ cafeASW/Kaffe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cafeASW
+{
+    public class Kaffe
+    {
+        public int Pris()
+        {
+            return 20;
+        }
+
+        public string Styrke()
+        {
+            return "Stærk.";
+        }
+
+    }
+}
---
+++ cafeASWTests/KaffeTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cafeASW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cafeASW.Tests
+{
+    [TestClass()]
+    public class KaffeTests
+    {
+        [TestMethod()]
+        public void PrisTestKaffe()
+        {
+            //arrange
+            var kaffe = new Kaffe();
+            //Act
+            int pris = kaffe.Pris();
+            //Assert
+            Assert.AreEqual(20, pris);
+        }
+
+        [TestMethod()]
+        public void StyrkeTestKaffe()
+        {
+            //arrange
+            var kaffe = new Kaffe();
+            //Act
+            string styrke = kaffe.Styrke();
+            //Assert
+            Assert.AreEqual("Stærk.", styrke);
+        }
+    }
+}
